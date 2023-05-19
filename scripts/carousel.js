7032127ec36d454cdc27d347f32e0364c121907d
Sand Despotism <wowow3722123@gmail.com>
2023-05-20 02:55:53 +0300

    cards added

---
+++ scripts/carousel.js
@@ -0,0 +1,33 @@
+let players = [
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Алексеев Андрей</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Бобажонов Темур</p><p id="role">Вратарь</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Виноградов Сергей</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Воденин Алексей</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Гилязетдинов Ринат</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Гречкин Кирилл</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Диняев Наиль</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Казанбаев Максим</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Колгин Андрей</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Кузьмин Денис</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Куликов Дмитрий</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Латыпов Артур</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Ледюков Евгений</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Лисиков Максим</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Манушкин Александр</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Мухановский Максим</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Нефёдов Эдуард</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Никитин Александр</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Никишин Александр</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Петров Сергей</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Сафронов Олег</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Семёнов Алексей</p><p id="role">Вратарь</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Фёдоров Владислав</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+  '<div class="card"><img src="" alt="Фото игрока"><p id="name">Шатрашанов Александр</p><p id="role">Универсал</p><button id="more">подробнее</button></div>',
+];
+let n_count = 1;
+function carousel(n) {
+  //   n -1 = left n 1 = right
+  n_count += n;
+  if (n_count == 2) {
+  }
+}
